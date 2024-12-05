5f6ae08dd4899106b91c284796fc89b8b693a91f
Ben Chess <bchess@gmail.com>
2024-12-04 16:44:25 -0800

    streaming and json

--- go.mod
+++ go.mod
@@ -5,17 +5,16 @@ go 1.22
 toolchain go1.22.10
 
 require (
+	github.com/golang/protobuf v1.5.4
 	go.etcd.io/etcd/api/v3 v3.5.17
 	go.etcd.io/etcd/client/v3 v3.5.17
 	google.golang.org/grpc v1.68.1
-	google.golang.org/protobuf v1.35.2
 )
 
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.17 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
@@ -25,4 +24,5 @@ require (
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
-	"reflect"
+	"os"
+	"time"
+	"unicode/utf8"
 
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
 	clientv3 "go.etcd.io/etcd/client/v3"
 
+	"github.com/golang/protobuf/jsonpb"
 	oldproto "github.com/golang/protobuf/proto" // Still used in etcd
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/encoding/prototext"
-	newproto "google.golang.org/protobuf/proto" // non-deprecated proto
+	// non-deprecated proto
 )
 
 // loggingUnaryInterceptor intercepts unary RPCs to log the decoded protobuf request and response.
@@ -26,23 +30,46 @@ func loggingUnaryInterceptor(
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler,
 ) (interface{}, error) {
-	log.Printf("Unary request - Method:%s\nRequest:\n%s", info.FullMethod, marshalProtoMessage(req))
-
-	// Log incoming metadata if needed
-	/*
-		md, ok := metadata.FromIncomingContext(ctx)
-		if ok {
-			log.Printf("Incoming metadata:\n%v", md)
-		}
-	*/
-
+	startTime := time.Now()
 	resp, err := handler(ctx, req)
-	if err != nil {
-		log.Printf("Unary error - Method:%s\nError:%v", info.FullMethod, err)
-	} else {
-		log.Printf("Unary response - Method:%s\nResponse:\n%s", info.FullMethod, marshalProtoMessage(resp))
+	duration := time.Since(startTime)
+
+	logEntry := LogEntry{
+		Timestamp: startTime.Format(time.RFC3339Nano),
+		Method:    info.FullMethod,
+		Type:      "unary",
+		Duration:  duration.String(),
+		Request:   marshalProtoMessage(req),
+		Response:  marshalProtoMessage(resp),
+		Error:     errorToString(err),
+		// Metadata:  metadataFromContext(ctx),
 	}
+
+	outputLogEntry(logEntry)
 	return resp, err
+}
+
+// loggingStreamInterceptor intercepts streaming RPCs to log the decoded protobuf messages.
+func loggingStreamInterceptor(
+	srv interface{},
+	ss grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) error {
+	startTime := time.Now()
+	err := handler(srv, &loggingStreamServer{ServerStream: ss, methodName: info.FullMethod})
+	duration := time.Since(startTime)
+
+	logEntry := LogEntry{
+		Timestamp: startTime.Format(time.RFC3339Nano),
+		Method:    info.FullMethod,
+		Type:      "stream",
+		Duration:  duration.String(),
+		Error:     errorToString(err),
+	}
+
+	outputLogEntry(logEntry)
+	return err
 }
 
 // loggingStreamServer intercepts streaming RPCs to log the decoded protobuf messages.
@@ -53,47 +80,108 @@ type loggingStreamServer struct {
 
 func (s *loggingStreamServer) RecvMsg(m interface{}) error {
 	err := s.ServerStream.RecvMsg(m)
-	if err == nil {
-		log.Printf("Stream recv - Method:%s\nMessage Type:%s\nMessage:\n%s", s.methodName, reflect.TypeOf(m), marshalProtoMessage(m))
-	} else if err == io.EOF {
-		log.Printf("Stream recv - Method:%s - EOF", s.methodName)
-	} else {
-		log.Printf("Stream recv error - Method:%s\nError:%v", s.methodName, err)
+	logEntry := LogEntry{
+		Timestamp: time.Now().Format(time.RFC3339Nano),
+		Method:    s.methodName,
+		Type:      "stream_recv",
+		Message:   marshalProtoMessage(m),
+		Error:     errorToString(err),
 	}
+	outputLogEntry(logEntry)
 	return err
 }
 
 func (s *loggingStreamServer) SendMsg(m interface{}) error {
-	log.Printf("Stream send - Method:%s\nMessage Type:%s\nMessage:\n%s", s.methodName, reflect.TypeOf(m), marshalProtoMessage(m))
-	return s.ServerStream.SendMsg(m)
-}
-
-func loggingStreamInterceptor(
-	srv interface{},
-	ss grpc.ServerStream,
-	info *grpc.StreamServerInfo,
-	handler grpc.StreamHandler,
-) error {
-	log.Printf("Stream started - Method:%s", info.FullMethod)
-	err := handler(srv, &loggingStreamServer{ServerStream: ss, methodName: info.FullMethod})
-	if err != nil {
-		log.Printf("Stream error - Method:%s\nError:%v", info.FullMethod, err)
-	} else {
-		log.Printf("Stream finished - Method:%s", info.FullMethod)
+	err := s.ServerStream.SendMsg(m)
+	logEntry := LogEntry{
+		Timestamp: time.Now().Format(time.RFC3339Nano),
+		Method:    s.methodName,
+		Type:      "stream_send",
+		Message:   marshalProtoMessage(m),
+		Error:     errorToString(err),
 	}
+	outputLogEntry(logEntry)
 	return err
 }
 
-// marshalProtoMessage converts a proto.Message to a human-readable string.
-func marshalProtoMessage(m interface{}) string {
-	switch msg := m.(type) {
-	case oldproto.Message:
-		return oldproto.MarshalTextString(msg)
-	case newproto.Message:
-		return prototext.Format(msg)
-	default:
-		return fmt.Sprintf("<not a proto.Message: %v>", m)
+// LogEntry represents a structured log entry
+type LogEntry struct {
+	Timestamp string            `json:"timestamp"`
+	Method    string            `json:"method"`
+	Type      string            `json:"type"`
+	Duration  string            `json:"duration,omitempty"`
+	Request   json.RawMessage   `json:"request,omitempty"`
+	Response  json.RawMessage   `json:"response,omitempty"`
+	Message   json.RawMessage   `json:"message,omitempty"`
+	Error     string            `json:"error,omitempty"`
+	Metadata  map[string]string `json:"metadata,omitempty"`
+}
+
+// outputLogEntry marshals a LogEntry to JSON and outputs it
+func outputLogEntry(entry LogEntry) {
+	data, err := json.Marshal(entry)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to marshal log entry: %v\n", err)
+		return
 	}
+	fmt.Println(string(data))
+}
+
+// marshalProtoMessage converts a proto.Message to a JSON-formatted byte slice.
+// It decodes base64-encoded bytes fields to UTF-8 strings for readability.
+func marshalProtoMessage(m interface{}) json.RawMessage {
+	if msg, ok := m.(oldproto.Message); ok {
+		marshaller := &jsonpb.Marshaler{}
+		str, err := marshaller.MarshalToString(msg)
+		if err != nil {
+			return json.RawMessage(`"<error marshaling message>"`)
+		}
+		// Unmarshal to map[string]interface{}
+		var data interface{}
+		if err := json.Unmarshal([]byte(str), &data); err != nil {
+			return json.RawMessage(`"<error unmarshaling JSON>"`)
+		}
+		// Decode base64-encoded strings
+		data = decodeBase64InMap(data)
+		// Marshal back to JSON
+		jsonBytes, err := json.Marshal(data)
+		if err != nil {
+			return json.RawMessage(`"<error marshaling JSON>"`)
+		}
+		return json.RawMessage(jsonBytes)
+	}
+	return nil
+}
+
+// decodeBase64InMap recursively traverses the data and decodes base64 strings.
+func decodeBase64InMap(data interface{}) interface{} {
+	switch v := data.(type) {
+	case map[string]interface{}:
+		for key, val := range v {
+			v[key] = decodeBase64InMap(val)
+		}
+		return v
+	case []interface{}:
+		for i, val := range v {
+			v[i] = decodeBase64InMap(val)
+		}
+		return v
+	case string:
+		decoded, err := base64.StdEncoding.DecodeString(v)
+		if err == nil && utf8.Valid(decoded) {
+			return string(decoded)
+		}
+		return v
+	default:
+		return v
+	}
+}
+
+func errorToString(err error) string {
+	if err != nil && err != io.EOF {
+		return err.Error()
+	}
+	return ""
 }
 
 func main() {
@@ -141,14 +229,10 @@ func main() {
 
 // registerEtcdServices registers the etcd services to the proxy server
 func registerEtcdServices(s *grpc.Server, conn *grpc.ClientConn) {
-	// Register KV service
 	etcdserverpb.RegisterKVServer(s, &kvServerProxy{client: etcdserverpb.NewKVClient(conn)})
-	// Register Watch service
 	etcdserverpb.RegisterWatchServer(s, &watchServerProxy{client: etcdserverpb.NewWatchClient(conn)})
-
+	etcdserverpb.RegisterLeaseServer(s, &leaseServerProxy{client: etcdserverpb.NewLeaseClient(conn)})
 	/*
-		// Register Lease service
-		etcdserverpb.RegisterLeaseServer(s, etcdserverpb.NewLeaseClient(conn))
 		// Register Cluster service
 		etcdserverpb.RegisterClusterServer(s, etcdserverpb.NewClusterClient(conn))
 		// Register Maintenance service
@@ -229,4 +313,71 @@ func (p *watchServerProxy) Watch(srv etcdserverpb.Watch_WatchServer) error {
 
 	// Wait for error
 	return <-errCh
+}
+
+// leaseServerProxy implements etcdserverpb.LeaseServer
+type leaseServerProxy struct {
+	client etcdserverpb.LeaseClient
+}
+
+// LeaseGrant forwards the LeaseGrant RPC
+func (p *leaseServerProxy) LeaseGrant(ctx context.Context, req *etcdserverpb.LeaseGrantRequest) (*etcdserverpb.LeaseGrantResponse, error) {
+	return p.client.LeaseGrant(ctx, req)
+}
+
+// LeaseRevoke forwards the LeaseRevoke RPC
+func (p *leaseServerProxy) LeaseRevoke(ctx context.Context, req *etcdserverpb.LeaseRevokeRequest) (*etcdserverpb.LeaseRevokeResponse, error) {
+	return p.client.LeaseRevoke(ctx, req)
+}
+
+// LeaseKeepAlive handles the bidirectional streaming RPC
+func (p *leaseServerProxy) LeaseKeepAlive(srv etcdserverpb.Lease_LeaseKeepAliveServer) error {
+	clientStream, err := p.client.LeaseKeepAlive(srv.Context())
+	if err != nil {
+		return err
+	}
+
+	errCh := make(chan error, 2)
+
+	// Client to Server
+	go func() {
+		for {
+			req, err := srv.Recv()
+			if err != nil {
+				errCh <- err
+				return
+			}
+			if err := clientStream.Send(req); err != nil {
+				errCh <- err
+				return
+			}
+		}
+	}()
+
+	// Server to Client
+	go func() {
+		for {
+			resp, err := clientStream.Recv()
+			if err != nil {
+				errCh <- err
+				return
+			}
+			if err := srv.Send(resp); err != nil {
+				errCh <- err
+				return
+			}
+		}
+	}()
+
+	return <-errCh
+}
+
+// LeaseTimeToLive forwards the LeaseTimeToLive RPC
+func (p *leaseServerProxy) LeaseTimeToLive(ctx context.Context, req *etcdserverpb.LeaseTimeToLiveRequest) (*etcdserverpb.LeaseTimeToLiveResponse, error) {
+	return p.client.LeaseTimeToLive(ctx, req)
+}
+
+// LeaseLeases forwards the LeaseLeases RPC
+func (p *leaseServerProxy) LeaseLeases(ctx context.Context, req *etcdserverpb.LeaseLeasesRequest) (*etcdserverpb.LeaseLeasesResponse, error) {
+	return p.client.LeaseLeases(ctx, req)
 }
